dab2931b0d7df6e9898626e04f28e1a13177a38c
FARHAN HOSSEIN <farhan.hossein@gmail.com>
2023-03-29 17:10:40 -0400

    second commit

---
+++ train.py
@@ -0,0 +1,15 @@
+import seaborn as sns
+import matplotlib.pyplot as plt
+import numpy as np;
+import tensorflow as tf
+import pandas as pd
+import sklearn
+import category_encoders as ce
+from tensorflow import keras
+from tensorflow.keras import layers
+
+
+
+np.set_printoptions(precision=3, suppress=True)
+
+print(tf.__version__)
